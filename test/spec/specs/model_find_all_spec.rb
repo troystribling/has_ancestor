f95c75b22dad124db988994819022268441f87ab
Troy Stribling <troy.stribling@gmail.com>
2007-08-11 03:58:05 +0000

    started work on enhanced finders

---
+++ test/spec/specs/model_find_all_spec.rb
@@ -0,0 +1,113 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+#########################################################################################################
+describe "database queries for all models of a specified type where the models have no ancestor and no descendants" do
+
+  before(:all) do
+    @p1 = ParentModel.new(model_data[:parent_model])
+    @p1.save
+    @p2 = ParentModel.new(model_data[:parent_model])
+    @p2.save
+  end
+
+  after(:all) do
+    @p1.destroy
+    @p2.destroy
+  end
+
+  it "should find models by specification of model attribute" do
+    ParentModel.find_all_by_parent_model_attr(model_data[:parent_model]['parent_model_attr']).should \
+      eql_attribute_value(:parent_model_attr, model_data[:parent_model]['parent_model_attr']) 
+  end
+
+end
+
+#########################################################################################################
+describe "database queries for all models of a specified type where the models have no ancestor and have descendants" do
+
+  before(:all) do
+    @c1 = ChildModel.new(model_data[:child_model])
+    @c1.save
+    @c2 = ChildModel.new(model_data[:child_model_find_all_child])
+    @c2.save
+  end
+
+  after(:all) do
+    @c1.destroy
+    @c2.destroy
+  end
+
+  it "should find models by specification of model attribute" do
+    ParentModel.find_all_by_parent_model_attr(model_data[:child_model]['parent_model_attr']).should \
+      eql_attribute_value(:parent_model_attr, model_data[:child_model]['parent_model_attr']) 
+  end
+
+end
+
+#########################################################################################################
+describe "database queries for all models of a specified type where the models have an ancestor and have no descendants" do
+
+  before(:all) do
+    @c = ChildModel.new(model_data[:child_model])
+    @c.save
+    @cp = ChildModel.new(model_data[:child_model_find_all_parent])
+    @cp.save
+    @cc = ChildModel.new(model_data[:child_model_find_all_child])
+    @cc.save
+  end
+
+  after(:all) do
+    @c.destroy
+    @cp.destroy
+    @cc.destroy
+  end
+
+  it "should find models by specification of model attribute" do
+    ChildModel.find_all_by_child_model_attr(model_data[:child_model]['child_model_attr']).should \
+      eql_attribute_value(:child_model_attr, model_data[:child_model]['child_model_attr']) 
+  end
+
+  it "should find models by specification of ancestor model attribute" do
+    ChildModel.find_all_by_parent_model_attr(model_data[:child_model]['parent_model_attr']).should \
+      eql_attribute_value(:parent_model_attr, model_data[:child_model]['parent_model_attr']) 
+  end
+
+end
+
+#########################################################################################################
+describe "database queries for all models of a specified type where the models have no descendants and have an ancestor has an ancestor" do
+
+  before(:all) do
+    @g = GrandchildModel.new(model_data[:grandchild_model])
+    @g.save
+    @gp = GrandchildModel.new(model_data[:grandchild_model_find_all_parent])
+    @gp.save
+    @gc = GrandchildModel.new(model_data[:grandchild_model_find_all_child])
+    @gc.save
+    @gg = GrandchildModel.new(model_data[:grandchild_model_find_all_grandchild])
+    @gg.save
+  end
+
+  after(:all) do
+    @g.destroy
+    @gp.destroy
+    @gc.destroy
+    @gg.destroy
+  end
+
+  it "should find models by specification of model attribute" do
+    GrandchildModel.find_all_by_grandchild_model_attr(model_data[:grandchild_model]['grandchild_model_attr']).should \
+      eql_attribute_value(:grandchild_model_attr, model_data[:grandchild_model]['grandchild_model_attr']) 
+  end
+
+  it "should find models by specification of ancestor attribute" do
+    GrandchildModel.find_all_by_child_model_attr(model_data[:grandchild_model]['child_model_attr']).should \
+      eql_attribute_value(:child_model_attr, model_data[:grandchild_model]['child_model_attr']) 
+  end
+
+  it "should find models by specification of ancestor's ancestor attribute" do
+    GrandchildModel.find_all_by_parent_model_attr(model_data[:grandchild_model]['parent_model_attr']).should \
+      eql_attribute_value(:parent_model_attr, model_data[:grandchild_model]['parent_model_attr']) 
+  end
+
+end
---
+++ test/spec/specs/model_find_by_multiple_attributes_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+################################ #################################################################
+#describe "database queries by multible attributes for a model where model does not have an ancestor and does not have descendants" do
+#
+#  before(:all) do
+#    @p = ParentModel.new(model_data[:parent_model_multiple_find])
+#    @p.save
+#  end
+#
+#  after(:all) do
+#    @p.destroy
+#  end
+#
+#  it "should find model by specification of mutiple model attribute match condition where all attributes belong to model" do
+#    ParentModel.find(:first, :conditions => "parent_model_attr = '#{model_data[:parent_model_multiple_find]['parent_model_attr']}' and parent_model_other_attr = '#{model_data[:parent_model_multiple_find]['parent_model_other_attr']}'").attributes.should \
+#      eql_attributes(model_data[:parent_model_multiple_find])
+#  end
+#
+#  it "should find model with generated finder which uses mutiple model attribute match condition where all attributes belong to model" do
+#    ParentModel.find_by_parent_model_attr_and_parent_model_other_attr(model_data[:parent_model_multiple_find]['parent_model_attr'], model_data[:parent_model_multiple_find]['parent_model_other_attr']).attributes.should \
+#      eql_attributes(model_data[:parent_model_multiple_find])
+#  end
+#
+#end
+#
+#################################################################################################
+describe "database queries by multible attributes for a model where model has an ancestor and does not have descendants" do
+
+  before(:all) do
+    @c = ChildModel.new(model_data[:child_model_multiple_find])
+    @c.save
+  end
+
+  after(:all) do
+    @c.destroy
+  end
+
+#  it "should find model by specification of mutiple model attribute match condition where all attributes belong to model" do
+#    ChildModel.find(:first, :conditions => "child_model_attr = '#{model_data[:child_model_multiple_find]['child_model_attr']}' and child_model_other_attr = '#{model_data[:child_model_multiple_find]['child_model_other_attr']}'").attributes.should \
+#      eql_attributes(model_data[:child_model_multiple_find])
+#  end
+#
+  it "should find model by specification of mutiple model attribute match condition where some attributes belong to model and other belong to ancestor" do
+    ChildModel.find(:first, 
+      :conditions => "child_models.child_model_attr = '#{model_data[:child_model_multiple_find]['child_model_attr']}' AND parent_models.parent_model_attr = '#{model_data[:child_model_multiple_find]['parent_model_attr']}'",
+      :joins => "LEFT JOIN parent_models ON parent_models.parent_model_descendant_id = child_models.child_model_id").attributes.should \
+      eql_attributes(model_data[:child_model_multiple_find])
+  end
+
+end
+
